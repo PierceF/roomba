4129fb5b28f96ef4e75170d109b1d23f5946a4d6
Pierce Franklin <piush_r@hotmail.com>
2020-10-06 14:08:54 -0500

    Updates

---
+++ precache-manifest.85d0efc585125e2bf8d8ffd1dc37f62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9446a5804017a7cb03fbb13af5c6d3b6",
+    "url": "/roomba/index.html"
+  },
+  {
+    "revision": "dce77a8c326f03cee63f",
+    "url": "/roomba/static/css/main.cb975818.chunk.css"
+  },
+  {
+    "revision": "53c321784adf21e7d4f0",
+    "url": "/roomba/static/js/2.d16ef026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roomba/static/js/2.d16ef026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce77a8c326f03cee63f",
+    "url": "/roomba/static/js/main.c1ce7b78.chunk.js"
+  },
+  {
+    "revision": "b85bdc1fdbabef741401",
+    "url": "/roomba/static/js/runtime-main.89a47f1a.js"
+  }
+]);
